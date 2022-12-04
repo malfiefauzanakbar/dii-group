a20c954b3101a3318eb8fa42affb741ead10c669
alfie <user@users-MacBook-Air.local>
2022-12-05 01:49:42 +0700

    test summernote

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -75,10 +75,30 @@ class ArticleController extends Controller
             $request->image->storeAs('public/article/', $imageName);
             // $request->image->move(public_path('article/'), $imageName);       
 
+            //upload image on description
+            $description = $request->description; 
+            $dom = new \DomDocument();        
+            $dom->loadHtml($description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);    
+            $images = $dom->getElementsByTagName('img');
+        
+            foreach($images as $k => $img){
+                $data = $img->getAttribute('src'); 
+                list($type, $data) = explode(';', $data); 
+                list($type, $data) = explode(',', $data); 
+                $data = base64_decode($data); 
+                $image_name= config('environment.app_url').config('environment.dir_article').time().$k.'.png'; 
+                $path = $image_name; 
+                file_put_contents($path, $data); 
+                $img->removeAttribute('src');
+                $img->setAttribute('src', $image_name);    
+            }
+        
+            $description = $dom->saveHTML();
+
             $article = Article::create([                                
                 'image'     => $imageName,
                 'title'      => $request->input('title'),
-                'description'      => $request->input('description'),                
+                'description'      => $description,
                 'status'      => $request->input('status'),
             ]);
 
